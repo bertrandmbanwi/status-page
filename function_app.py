9a8e6960d4e16a82b3cbc7f42165bfbc03e53010
bertrandmbanwi <bertrandmbanwi@gmail.com>
2023-11-06 12:17:48 -0500

    test-4

--- function_app.py
+++ function_app.py
@@ -7,7 +7,7 @@ import json
 
 app = func.FunctionApp()
 
-@app.schedule(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=True,
+@app.schedule(schedule="0 */2 * * * *", arg_name="myTimer", run_on_startup=True,
               use_monitor=False) 
 def timer_trigger(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
